782c9022bd88a71c8c3608ec19e89dbc2471acd4
Carlos G <rhcp021@gmail.com>
2018-08-05 11:13:05 -0500

    Activity context y ciclos de vida

--- app/src/main/java/com/android/krlos/firstapp/MainActivity.java
+++ app/src/main/java/com/android/krlos/firstapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.android.krlos.firstapp;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,6 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"First app!!",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"onCreate",Toast.LENGTH_LONG).show();
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart",Toast.LENGTH_LONG).show();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume",Toast.LENGTH_LONG).show();
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"onPause",Toast.LENGTH_LONG).show();
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop",Toast.LENGTH_LONG).show();
+    }
+
+    @Override protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"onRestar",Toast.LENGTH_LONG).show();
+    }
+
+    @Override protected void onDestroy() {
+        Toast.makeText(this,"onDestroy",Toast.LENGTH_LONG).show();
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/android/krlos/firstapp/test.java
@@ -0,0 +1,10 @@
+package com.android.krlos.firstapp;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class test {
+    public static void myT(Context context){
+        Toast.makeText(context,"Toast desde clase test",Toast.LENGTH_LONG).show();
+    }
+}
